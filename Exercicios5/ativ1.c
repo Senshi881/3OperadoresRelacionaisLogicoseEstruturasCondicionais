5a1802102688b03f60418ac5eea9607994a82dfb
Senshi881 <caiogamer881@gmail.com>
2023-09-05 22:38:36 -0300

    AdiçãoAtualização de novos arquivos

--- Exercicios5/ativ1.c
+++ Exercicios5/ativ1.c
@@ -1,22 +1,22 @@
-//6. Codifique um programa que lê um número inteiro. A seguir o programa deve imprimir uma mensagem para o usuário dizendo se o número
-//digitado é par. Se o número não for par, o programa não deve fazer nada.
-#include <stdio.h>
+// 6. Codifique um programa que lê um número inteiro. A seguir o programa deve
+// imprimir uma mensagem para o usuário dizendo se o número digitado é par. Se o
+// número não for par, o programa não deve fazer nada.
 #include <math.h>
+#include <stdio.h>
 
-int main(){
-    int numInt;
+int main() {
+  int numInt;
 
-    printf("Me informe o numero para verificarmos se ele e par ou nao: ");
-    scanf("%d", &numInt);
-    while (getchar() != '\n');
+  printf("Me informe o numero para verificarmos se ele e par ou nao: ");
+  scanf("%d", &numInt);
+  while (getchar() != '\n')
+    ;
 
+  if (numInt % 2 == 0) {
+    printf("Seu numero e par;");
+  } else {
+    printf("Nao e par;");
+  }
 
-    if(numInt % 2 == 0){
-        printf("Seu numero e par;");
-    } else {
-        printf("Nao e impar;");
-    }
-
-
-    return 0;
+  return 0;
 }
